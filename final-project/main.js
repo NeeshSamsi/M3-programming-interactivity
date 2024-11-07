965322fdd5fc1a953c6234b97158aadc194f3cce
Neesh <avaneeshsamsi@gmail.com>
2024-11-07 13:20:18 +0100

    feat: Footer & interactive canvas

--- final-project/main.js
+++ final-project/main.js
@@ -10,3 +10,36 @@ faqs.forEach((faq) => {
     }
   })
 })
+
+const canvas = document.getElementById("interactive-canvas")
+const ctx = canvas.getContext("2d")
+canvas.width = window.innerWidth - 300
+canvas.height = window.innerHeight - 300
+
+console.log(window.innerWidth - 160, window.innerHeight - 160)
+
+let isMouseDown = false
+
+canvas.addEventListener("mousedown", () => {
+  ctx.beginPath()
+  isMouseDown = true
+})
+
+canvas.addEventListener("mouseup", () => {
+  isMouseDown = false
+})
+
+canvas.addEventListener("mousemove", (event) => {
+  const rect = canvas.getBoundingClientRect()
+  const mouseX = event.clientX - rect.left
+  const mouseY = event.clientY - rect.top
+
+  if (isMouseDown) {
+    drawLine(mouseX, mouseY)
+  }
+})
+
+function drawLine(x, y) {
+  ctx.lineTo(x, y)
+  ctx.stroke()
+}
